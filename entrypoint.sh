dd31b6f5ee0ac0e387001f95fc3c972068f45d03
aefraimidis <87641295+aefraimidis@users.noreply.github.com>
2021-09-20 13:03:57 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,15 +22,11 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 
 INPUT_SOURCE_FILE=`ls -A | egrep -v '^(.htaccess|class.db_com.php|config.php)'`
 
-cp -R $INPUT_SOURCE_FILE "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+cp -Rf $INPUT_SOURCE_FILE "$CLONE_DIR
 cd "$CLONE_DIR"
-
-ls -lrth 
 
 if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
 then
