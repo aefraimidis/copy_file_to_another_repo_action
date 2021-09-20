ccab719267d62f2e8b27dc40ea78c66862ad76b0
aefraimidis <87641295+aefraimidis@users.noreply.github.com>
2021-09-20 15:20:26 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-INPUT_SOURCE_FILE=`ls -A | egrep -v '^($INPUT_COMMIT_MESSAGE")'`
+INPUT_SOURCE_FILE=`ls -A | egrep -v '^($INPUT_IGNORE_FILES)'`
 
 cp -Rf $INPUT_SOURCE_FILE "$CLONE_DIR"
 cd "$CLONE_DIR"
@@ -42,7 +42,7 @@ echo "Adding git commit"
 git add .
 if git status | grep -q "Changes to be committed"
 then
-  git commit --message "push from dev"
+  git commit --message xc h from dev"
   echo "Pushing git commit"
   git push -u origin HEAD:$OUTPUT_BRANCH
 else
